ce30e29924815bd608570dabcba329c97cbff6cb
suyeong <suyeong@zezedu.com>
2022-04-20 00:57:18 +0900

    Feat: aws 서버와 데이터베이스 연동

--- server/models/font.js
+++ server/models/font.js
@@ -5,18 +5,22 @@ sequelize.define(
     "Font",
     {
         name:{
-            type: DataTypes.STRING(32),
+            type: DataTypes.STRING(15),
             allowNull: false,
         },
         description: {
-            type: DataTypes.STRING(120),
+            type: DataTypes.STRING(150),
+            allowNull: true,
+        },
+        corporation: {
+            type: DataTypes.STRING(25),
             allowNull: true,
         }
-
     },
     {
         timestamps: false,
         freezeTableName: true,
+        charset: "utf8"
     }
 );
 
