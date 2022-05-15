9dd468875ee85799e0b9800ef2843e61149ea2d7
suyeong <suyeong@zezedu.com>
2022-05-15 19:45:46 +0900

    Feat: Main, FontTagAdmin 분리

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import styled from "styled-components";
 import { NavBar } from "./style";
 import { DashLineHorizontal } from "../globalStyle";
@@ -6,7 +7,9 @@ import { DashLineHorizontal } from "../globalStyle";
 export default function Navigation() {
   return (
     <NavBar>
-      <div>TagType</div>
+      <Link href="/" passHref>
+        <button>TagType</button>
+      </Link>
       <div>Before memorizing the names and shapes of 10,000 fonts, I'm glad we were able to create this site.</div>
       <DashLineHorizontal />
       <div>Home</div>
@@ -14,6 +17,10 @@ export default function Navigation() {
       <div>TagType</div>
       <DashLineHorizontal />
       <div>TagSearch</div>
+      <DashLineHorizontal />
+      <Link href="/admin-font-tag" passHref>
+        <button>관리자 페이지</button>
+      </Link>
       <DashLineHorizontal />
       bistro logo
     </NavBar>
