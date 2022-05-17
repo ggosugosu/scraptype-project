0990bb1e913149517caec0e8e1f4876775ecc18c
suyeong <suyeong@zezedu.com>
2022-05-18 03:14:28 +0900

    Feat: 아카이브 페이지 샘플 제작

--- client/src/components/Archive/index.tsx
+++ client/src/components/Archive/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import ArchiveItem from "../../features/ArchiveItem";
+
+const sampleList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const ArchiveWrapper = styled.div`
+  display: flow-root;
+`;
 
 export default function Archive() {
   return (
-    <div>main archive</div>
-  )
+    <ArchiveWrapper>
+      {sampleList.map((item, index) => (
+        <ArchiveItem key={index} />
+      ))}
+    </ArchiveWrapper>
+  );
 }
