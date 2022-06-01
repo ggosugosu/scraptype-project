ac68a3c9c018b7d03c305b48a6e2ea2dc676083d
suyeong <suyeong@zezedu.com>
2022-06-02 05:35:36 +0900

    Feat: 모바일형 아카이브 제목 추가

--- client/src/components/Archive/index.tsx
+++ client/src/components/Archive/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Image from "next/image";
 import styled from "styled-components";
 import ArchiveItem from "../../features/ArchiveItem";
-
+import logo from "../../assets/images/logo_no_icon.svg";
+import logoBistro from "../../assets/images/logo_bistro.svg";
+import { grey_200 } from "../colors";
 const sampleList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const ArchiveWrapper = styled.div`
   display: inline-flex;
@@ -15,16 +18,37 @@ const ArchiveWrapper = styled.div`
   @media (max-width: 480px) {
     justify-content: center;
     gap: 8px;
-    padding: 8px 0;
+    padding: 24px 0;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  display: none;
+  width: 100%;
+  height: 188px;
+  border-bottom: 4px dotted ${grey_200};
+
+  @media (max-width: 480px) {
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
   }
 `;
 
 export default function Archive() {
   return (
-    <ArchiveWrapper>
-      {sampleList.map((item, index) => (
-        <ArchiveItem key={index} />
-      ))}
-    </ArchiveWrapper>
+    <>
+      <LogoWrapper>
+        <Image src={logo} alt="logo" />
+        <Image src={logoBistro} alt="logo_bistro" width="72" height="42" />
+      </LogoWrapper>
+      <ArchiveWrapper>
+        {sampleList.map((item, index) => (
+          <ArchiveItem key={index} />
+        ))}
+      </ArchiveWrapper>
+    </>
   );
 }
