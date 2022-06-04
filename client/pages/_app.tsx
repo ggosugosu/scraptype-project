15bd82afc1d5f6067461ff294388fcc5ebff2fbd
suyeong <suyeong@zezedu.com>
2022-06-05 04:31:32 +0900

    Fix: 환경변수 제대로 동작하지 않는 현상 수정

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,11 +2,11 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Layout from "../src/components/Layout";
-import { GlobalStyle } from "../src/components/globalStyle";
+import { GlobalStyle } from "../src/common/globalStyle";
 import "../src/assets/fonts/fonts.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({ uri: `http://${process.env.HOST}`, cache: new InMemoryCache() });
+  const client = new ApolloClient({ uri: `${process.env.NEXT_PUBLIC_HOST}`, cache: new InMemoryCache() });
   return (
     <ApolloProvider client={client}>
       <RecoilRoot>
