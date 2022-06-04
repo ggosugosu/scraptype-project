4822cec8e74a091f0c97e9ba20db4e8a52751ec2
suyeong <suyeong@zezedu.com>
2022-06-05 03:22:04 +0900

    Feat: 페이지 기본 헤더 / 태그검색 아이템 샘플  생성

--- client/src/components/SearchTag/index.tsx
+++ client/src/components/SearchTag/index.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styled from "styled-components";
+import back from "../../assets/images/ic_back.svg";
+import { main_light } from "../colors";
+
+interface Props {
+  title: String;
+}
+
+interface ItemProps {
+  name: String;
+  selected: Boolean;
+}
+
+const Title = styled.header`
+  display: flex;
+  flex: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
+  height: 48px;
+
+  span {
+    font-family: "Gothic-M";
+    font-size: 32px;
+  }
+`;
+
+const Item = styled.div<{ selected: Boolean }>`
+  display: inline-block;
+  height: 28px;
+  font-size: 20px;
+  box-shadow: ${({ selected }) => (selected ? `inset 0 -4px 0 ${main_light}` : ``)};
+`;
+
+function PageTitle(props: Props) {
+  return (
+    <Title>
+      <Image src={back} alt="back" />
+      <span>{props.title}</span>
+    </Title>
+  );
+}
+
+function TagSearchBox() {}
+
+function TagSearchItem(props: ItemProps) {
+  return <Item selected={props.selected}>{props.name}</Item>;
+}
 
 export default function SearchTag() {
   return (
-    <div>Search Tag</div>
+    <>
+      <PageTitle title="Search Tag" />
+      <TagSearchItem name="테스트" selected={true} />
+    </>
   );
 }
--- client/src/components/colors.ts
+++ client/src/components/colors.ts
@@ -7,7 +7,13 @@ export const grey_200 = `#D5D6D8`;
 export const grey_100 = `#F1F1F1`;
 export const white = `#FFFFFF`;
 
-
 export const main_light = `#6FFFCB`;
 export const main = `#19D29A`;
+
+export const texas_rose = `#FFB15B`;
+export const hot_pink = `#FF5CC4`;
+export const torea_bay = `#0836A6`;
+export const cerulean = `#009CCF`;
+export const java = `#1AE4A7`;
+export const turbo = `#FFEA00`;
 
