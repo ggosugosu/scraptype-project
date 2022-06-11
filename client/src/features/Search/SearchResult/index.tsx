1e97723a5eb117a8684ea138f4fe454799d8101d
suyeong <suyeong@zezedu.com>
2022-06-11 23:50:56 +0900

    Chore: 불필요한 코드 삭제 및 간소화

--- client/src/features/Search/SearchResult/index.tsx
+++ client/src/features/Search/SearchResult/index.tsx
@@ -9,18 +9,14 @@ interface Props {
 const toArray = (keywords: string) => keywords.split(",").map(Number);
 
 export default function SearchResult(props: Props) {
-  console.log(`array: ${toArray(props.keywords)}`);
   const { loading, error, data } = useQuery(GET_FONTS_BY_TAG_ID, { variables: { tag_ids: toArray(props.keywords) } });
-  console.log(`${JSON.stringify(data)}`);
 
-  if (data === null || loading) return <div>loading...</div>;
+  if (loading) return <div>loading...</div>;
   if (error) return <div>{error.message}</div>;
-
-  console.log(`${JSON.stringify(data.getFontsByTagId)}`);
 
   return (
     <>
-      {data.getFontsByTagId.map((font, index) => 
+      {data && data.getFontsByTagId.map((font, index) => 
         (
           <div key={index}>
             {font.name}
