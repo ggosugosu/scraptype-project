ad8f46f0af052643452489168cb080d5530b0924
suyeong <suyeong@zezedu.com>
2022-06-17 04:10:27 +0900

    Style: 모델, 스타일 분리

--- client/src/features/Archive/ArchiveItem/index.tsx
+++ client/src/features/Archive/ArchiveItem/index.tsx
@@ -1,19 +1,11 @@
-import styled, { withTheme } from "styled-components";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { filterFontFamily, getSvgUrl } from "features/utils";
 import InjectFontFace from "components/InjectFontFace";
 import ArchiveSVG from "assets/images/ic_archive.svg";
 import ArchiveBarcodeSVG from "assets/images/ic_archive_barcode.svg";
-import { black, torea_bay, white } from "common/colors";
-
-
-interface ItemColor {
-  text: string;
-  background: string;
-  barcode: string;
-}
-
+import { charList, colorList, ItemColor } from "./models";
+import { CharBox } from "./styled";
 interface Props {
   id: number;
   name: string;
@@ -27,45 +19,6 @@ export interface WebFont {
   family: string;
   source: string;
 }
-
-const charList = ["담", "삭", "영", "벗", "만", "리", "재", "굿", "비", "스", "트", "로"];
-
-const colorList: ItemColor[] = [
-  { text: `${black}`, background: "texas_rose", barcode: "black" },
-  { text: `${white}`, background: "hot_pink", barcode: "white" },
-  { text: `${white}`, background: "torea_bay", barcode: "white" },
-  { text: `${white}`, background: "cerulean", barcode: "white" },
-  { text: `${torea_bay}`, background: "java", barcode: "torea_bay" },
-  { text: `${torea_bay}`, background: "turbo", barcode: "torea_bay" },
-];
-
-const CharBox = styled.button<{ selectedColor: ItemColor; fontFamily: string }>`
-  position: relative;
-  width: 168px;
-  height: 168px;
-  span {
-    position: relative;
-    top: 5px;
-    left: 0;
-    font-family: ${({ fontFamily }) => fontFamily}, sans-serif;
-    font-size: 100px;
-    line-height: 90px;
-    color: ${({ selectedColor }) => selectedColor.text || "gray"};
-  }
-
-  .barcode_wrapper {
-    position: absolute;
-    left: 16px;
-    bottom: 16px;
-  }
-
-  @media (max-width: 480px) {
-    flex: 0 1 calc(50vw - 20px);
-    height: calc(50vw - 20px);
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-`;
 
 export default function ArchiveItem({ id, name, description, corporation, tags, webFonts }: Props) {
   const [char, setChar] = useState<String>(charList[0]);
@@ -82,7 +35,7 @@ export default function ArchiveItem({ id, name, description, corporation, tags, 
         <Image alt="button-text" src={ArchiveSVG} layout="fill" className={`filter_${color.background}`} />
         <span>{char}</span>
         <div className="barcode_wrapper">
-        <Image alt="button-barcode" src={ArchiveBarcodeSVG} width="36" height="36" className={`filter_${color.barcode}`} />
+          <Image alt="button-barcode" src={ArchiveBarcodeSVG} width="36" height="36" className={`filter_${color.barcode}`} />
         </div>
       </CharBox>
     </>
