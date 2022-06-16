292f302c088fa20e45451f9abc9abeb268fa3ce7
suyeong <suyeong@zezedu.com>
2022-06-17 04:23:50 +0900

    Feat: 검색 결과 페이지 description 추가 및 레이아웃 보정

--- client/src/features/Search/SearchResult/SearchResultItem/style.ts
+++ client/src/features/Search/SearchResult/SearchResultItem/style.ts
@@ -43,7 +43,7 @@ export const ItemContent = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
   height: 88px;
   padding-left: 24px;
