71f752309317133359f51050af2f9a070ac83135
suyeong <suyeong@zezedu.com>
2022-06-18 17:10:21 +0900

    Feat: 레이아웃 보정

--- client/src/features/Search/SearchFont/index.tsx
+++ client/src/features/Search/SearchFont/index.tsx
@@ -1,84 +1,12 @@
 import { useQuery } from "@apollo/client";
-import { black, main } from "common/colors";
 import { ButtonPositive } from "components/Button";
 import PageTitle from "components/PageTitle";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
-import styled from "styled-components";
 import { GET_CORPORATION_ALL } from "./gql";
 import ArrowDownSVG from "assets/images/ic_arrow_down.svg";
-
-const SearchWrapper = styled.div`
-display: flex;
-flex-flow: row wrap;
-gap: 16px;
-`;
-const BoxWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  width: auto;
-  height: auto;
-  .arrow_wrapper {
-    position: absolute;
-    right: 16px;
-    display: flex;
-    align-items: center;
-    width: 32px;
-    height: 54px;
-  }
-`;
-const Corporation = styled.select`
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  position: relative;
-  width: 270px;
-  height: 54px;
-  border: none;
-  border-bottom: solid 2px ${black};
-  padding: 0 16px;
-  background-color: transparent;
-  font-size: 20px;
-
-  select {
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-  }
-
-  &:focus {
-    outline: none;
-    border-bottom: solid 2px ${main};
-  }
-
-  option {
-    border: 1px solid ${main};
-    padding: 16px;
-    font-size: 20px;
-  }
-`;
-
-const Font = styled.input.attrs((props) => ({
-  type: "text",
-}))`
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  border: none;
-  border-bottom: solid 2px ${black};
-  flex: 1 1 auto;
-  width: 0px;
-  height: 54px;
-  font-size: 20px;
-
-  &:focus {
-    outline: none;
-    border-bottom: solid 2px ${main};
-  }
-`;
-
-function SelectBox() {}
+import { FlexWrapper, SelectorWrapper, Selector, Text } from "./style";
 
 export default function SearchFont() {
   const router = useRouter();
@@ -97,36 +25,37 @@ export default function SearchFont() {
   if (loading || error) return null;
 
   return (
-    <section>
-      <PageTitle title="Search Font" onClick={() => router.push("/")} />
-      <SearchWrapper>
-      <BoxWrapper>
-        <div className="arrow_wrapper">
-          <Image alt="arrow" src={ArrowDownSVG} layout="fill" />
-        </div>
-        <Corporation value={corporation} onChange={handleCorporationChange}>
-          <option value={""}>제작사 전체</option>
-          {data &&
-            data.getCorporationAll.map((item, index) => (
-              <option key={index} value={item.corporation}>
-                {item.corporation}
-              </option>
-            ))}
-        </Corporation>
-      </BoxWrapper>
-
-      <Font placeholder={"Enter the name of the font you want"} value={text} onChange={handleTextChange} />
-      <ButtonPositive
-        enabled={true}
-        text="SEARCH"
-        onClick={() =>
-          router.push({
-            pathname: "/search/font/result",
-            query: { type: "font", corporation: `${corporation}`, text: `${text}` },
-          })
-        }
-      />
-      </SearchWrapper>
+    <>
+      <PageTitle title="Name search" onClick={() => router.push("/")} />
+      <section>
+      <FlexWrapper>
+        <SelectorWrapper>
+          <div className="arrow_wrapper">
+            <Image alt="arrow" src={ArrowDownSVG} layout="fill" />
+          </div>
+          <Selector value={corporation} onChange={handleCorporationChange}>
+            <option value={""}>제작사 전체</option>
+            {data &&
+              data.getCorporationAll.map((item, index) => (
+                <option key={index} value={item.corporation}>
+                  {item.corporation}
+                </option>
+              ))}
+          </Selector>
+        </SelectorWrapper>
+        <Text placeholder={"Enter the name of the font you want"} value={text} onChange={handleTextChange} />
+        <ButtonPositive
+          enabled={true}
+          text="SEARCH"
+          onClick={() =>
+            router.push({
+              pathname: "/search/font/result",
+              query: { type: "font", corporation: `${corporation}`, text: `${text}` },
+            })
+          }
+        />
+      </FlexWrapper>
     </section>
+    </>
   );
 }
