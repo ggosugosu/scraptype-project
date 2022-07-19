bda149abdbfcc267646fbab27e67161fcb87437a
gosugosu <gohsue0@gmail.com>
2022-07-20 02:46:55 +0900

    Refactor: form state 오브젝트로 변경

--- client/src/features/Admin/Font/Form/FontForm.tsx
+++ client/src/features/Admin/Font/Form/FontForm.tsx
@@ -6,7 +6,7 @@ import { GridDivider, GridLayout } from 'components/Grid/style';
 import InputText from 'components/InputText';
 import InputTextArea from 'components/InputTextArea';
 import Radio from 'components/Radio';
-import { useEffect, useMemo, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 import { GET_FONT_BY_FONT_ID } from './gql';
 import ImageFont from './ImageFont';
 import WebFont from './WebFont';
@@ -17,52 +17,66 @@ type Props = {
 
 type FormData = {
   name: string;
-  corporation?: string;
-  description?: string;
+  corporation: string;
+  description: string;
   isWebFont: boolean;
-  webFontData: WebFontData;
-  imageFontData: ImageFontData;
+  webFont: WebFont;
+  imageFont: ImageFont;
 };
 
-type WebFontData = {
-  source: string
-}
+type WebFont = {
+  source: string;
+};
 
-type ImageFontData = {
+type ImageFont = {
   title: string;
   unit: string;
   detailMob: string;
   detailPc: string;
-}
+};
+
+const initFontForm: FormData = {
+  name: ``,
+  corporation: ``,
+  description: ``,
+  isWebFont: true,
+  webFont: { source: `` },
+  imageFont: { title: ``, unit: ``, detailMob: ``, detailPc: `` },
+};
 
 const FontForm = ({ font_id }: Props) => {
   const { data } = useQuery(GET_FONT_BY_FONT_ID, { variables: { font_id: Number(font_id) } });
   const isCreate = useMemo(() => font_id === 'create', [font_id]);
-  const [font, setFont] = useState<string>('');
-  const [corporation, setCorporation] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
-  const [isWebFont, setIsWebFont] = useState<boolean>(true);
+  const [formData, setFormData] = useState<FormData>(initFontForm);
 
   useEffect(() => {
     if (isCreate) return;
-    setFont(data?.getFontByFontId.name);
-    setCorporation(data?.getFontByFontId.corportation);
-    setDescription(data?.getFontByFontId.description);
-    setIsWebFont(data?.getFontByFontId.isWebFont);
+    const font = data?.getFontByFontId;
+    const imageFont = font?.imageFont ?? ``;
+    setFormData({
+      name: font?.name,
+      corporation: font?.corporation,
+      description: font?.description,
+      isWebFont: font?.isWebFont,
+      webFont: font?.webFont?.source ?? ``,
+      imageFont: { title: imageFont.title, unit: imageFont.unit, detailMob: imageFont.detail_mobile, detailPc: imageFont.detail_pc },
+    });
   }, [data]);
 
-  const handleFontChange = (e) => {
-    setFont(e.target.value);
+  const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
   };
-  const handleCorporationChange = (e) => {
-    setCorporation(e.target.value);
+
+  const handleIsWebFont = (isWebFont: boolean) => {
+    setFormData((prev) => ({
+      ...prev,
+      isWebFont,
+    }));
   };
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
-  };
-  const handleFontType = (isWebFont: boolean) => {
-    setIsWebFont(isWebFont);
-  };
+
   const handleSubmit = (e: SubmitEvent) => {
     e.preventDefault();
   };
@@ -75,17 +89,17 @@ const FontForm = ({ font_id }: Props) => {
             <label htmlFor="name">
               Font name<span className="required"> *</span>
             </label>
-            <InputText id="name" placeholder="text" value={font} onChange={handleFontChange} />
+            <InputText id="name" placeholder="text" value={formData.name} onChange={handleTextChange} />
           </GridLayout>
           <GridLayout>
-            <label>
+            <label htmlFor="corporation">
               Corporation<span className="required"> *</span>
             </label>
-            <InputText placeholder="text" value={corporation} onChange={handleCorporationChange} />
+            <InputText id="corporation" placeholder="text" value={formData.corporation} onChange={handleTextChange} />
           </GridLayout>
           <GridLayout row={`1 / span 2`} column={`2 / span 1`}>
             <label htmlFor="description">Memo</label>
-            <InputTextArea id="description" placeholder="text" value={description} onChange={handleDescriptionChange} />
+            <InputTextArea id="description" placeholder="text" value={formData.description} onChange={handleTextChange} />
           </GridLayout>
         </Grid>
         <GridDivider />
@@ -95,17 +109,17 @@ const FontForm = ({ font_id }: Props) => {
               어떤 유형의 폰트를 추가하시겠습니까?<span className="required"> *</span>
             </label>
             <Radio>
-              <input type="radio" name="fontType" id="webFont" value="true" checked={isWebFont} onChange={() => handleFontType(true)} />
+              <input type="radio" name="isWebFont" id="webFont" value="true" checked={formData.isWebFont} onClick={() => handleIsWebFont(true)} />
               <label htmlFor="webFont">웹 폰트</label>
             </Radio>
             <Radio>
-              <input type="radio" name="fontType" id="imageFont" value="false" checked={!isWebFont} onChange={() => handleFontType(false)} />
+              <input type="radio" name="isWebFont" id="imageFont" value="false" checked={!formData.isWebFont} onClick={() => handleIsWebFont(false)} />
               <label htmlFor="imageFont">이미지 폰트</label>
             </Radio>
           </GridLayout>
         </Grid>
         <GridDivider />
-        {isWebFont ? <WebFont /> : <ImageFont />}
+        {formData.isWebFont ? <WebFont /> : <ImageFont />}
         <GridDivider />
         <Grid template={`1fr`} padding={`36px 24px 56px 24px`}>
           <ButtonPositive enabled={true} text={`폰트 추가`} onClick={() => {}} />
