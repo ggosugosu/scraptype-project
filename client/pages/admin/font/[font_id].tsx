987a141d9d74f52356e60c905bf8786e38091e93
gosugosu <gohsue0@gmail.com>
2022-08-07 17:08:33 +0900

    Feat: PageTitle 추가

--- client/pages/admin/font/[font_id].tsx
+++ client/pages/admin/font/[font_id].tsx
@@ -1,3 +1,4 @@
+import PageTitle from 'components/PageTitle';
 import { FontForm, Overview } from 'features/Admin/Font';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -5,5 +6,12 @@ import React from 'react';
 export default function Font() {
   const router = useRouter();
   const { font_id } = router.query;
-  return <FontForm font_id={font_id?.toString() ?? `create`} />
+  console.log(font_id);
+
+  return (
+    <>
+      <PageTitle title={font_id?.toString() !== 'create' ? 'Edit' : 'Add'} onClick={() => router.back()} />
+      <FontForm font_id={font_id?.toString() ?? `create`} />
+    </>
+  );
 }
