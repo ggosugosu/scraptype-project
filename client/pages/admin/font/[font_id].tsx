cd810259fd479ee753018d56eb023d6039a9031b
ggosugosu <gohsue0@gmail.com>
2022-09-26 02:07:22 +0900

    Bugfix: build 안 되는 문제 해결

--- client/pages/admin/font/[font_id].tsx
+++ client/pages/admin/font/[font_id].tsx
@@ -1,26 +1,25 @@
 import PageTitle from "components/PageTitle";
-import { FontForm, Overview } from "features/Admin/Font";
-import { DeleteFontButton } from "features/Admin/Font/Form/FontForm";
-import { useRouter } from "next/router";
+import {FontForm} from "features/Admin/Font";
+import {DeleteFontButton} from "features/Admin/Font/Form/FontForm";
+import {useRouter} from "next/router";
 import React from "react";
 
-export default function Font() {
-  const router = useRouter();
-  const { font_id } = router.query ?? "create";
-  console.log(font_id);
+export default function AdminFontPage() {
+    const router = useRouter();
+    const font_id = router.query['font_id'] ?? "create";
 
-  return (
-    <>
-      <PageTitle
-        title={font_id?.toString() !== "create" ? "Edit" : "Add"}
-        endItem={
-          font_id?.toString() !== "create" && (
-            <DeleteFontButton font_id={font_id!!.toString()} />
-          )
-        }
-        onClick={() => router.back()}
-      />
-      <FontForm font_id={font_id?.toString() ?? `create`} />
-    </>
-  );
+    return (
+        <>
+            <PageTitle
+                title={font_id?.toString() !== "create" ? "Edit" : "Add"}
+                endItem={
+                    font_id?.toString() !== "create" && (
+                        <DeleteFontButton font_id={font_id!!.toString()}/>
+                    )
+                }
+                onClick={() => router.back()}
+            />
+            <FontForm font_id={font_id?.toString() ?? `create`}/>
+        </>
+    );
 }
