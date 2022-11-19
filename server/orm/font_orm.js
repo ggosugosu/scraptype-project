641f86152f848ca04af31b3f7884456a20c7735e
ggosugosu <gohsue0@gmail.com>
2022-11-20 04:02:16 +0900

    feat: imageFont 연동

--- server/orm/font_orm.js
+++ server/orm/font_orm.js
@@ -1,134 +1,138 @@
-const { Font, FontTag, Tag, WebFont, ImageFont } = require('../models/index');
-const { Op } = require('sequelize');
-const sequelize = require('sequelize');
+const { Font, FontTag, Tag, WebFont, ImageFont } = require("../models/index");
+const { Op } = require("sequelize");
+const sequelize = require("sequelize");
 
 const fontORM = {
-    getFontByFontId: ({ font_id }) =>
-        Font.findOne({
-            where: { id: font_id },
-            include: [
-                {
-                    model: FontTag,
-                    as: 'fontTags',
-                    include: [
-                        {
-                            model: Tag,
-                            as: 'tags',
-                        },
-                    ],
-                },
-                {
-                    model: WebFont,
-                    as: 'webFont',
-                },
-                {
-                    model: ImageFont,
-                    as: 'imageFont',
-                },
-            ],
-        }).then((data) => data),
-
-    getFontAll: () => {
-        const getFonts = Font.findAll({
-            include: [
-                {
-                    model: FontTag,
-                    as: 'fontTags',
-                    include: [
-                        {
-                            model: Tag,
-                            as: 'tags',
-                        },
-                    ],
-                },
-                {
-                    model: WebFont,
-                    as: 'webFont',
-                },
-            ],
-        });
-        return getFonts;
-    },
-
-    getFontsByTagId: ({ tag_ids }) => {
-        const getFonts = Font.findAll({
-            include: [
-                {
-                    model: FontTag,
-                    as: 'fontTags',
-                    include: [
-                        {
-                            model: Tag,
-                            as: 'tags',
-                        },
-                    ],
-                    where: {
-                        tag_id: {
-                            [Op.in]: tag_ids,
-                        },
-                    },
-                },
-            ],
-        });
-
-        return getFonts;
-    },
-
-    getFontsByCorpAndText: ({ corporation, text }) =>
-        Font.findAll({
-            where: {
-                name: {
-                    [Op.like]: `%${text}%`,
-                },
-                corporation: {
-                    [Op.like]: `${corporation ? corporation : '%'}`,
-                },
+  getFontByFontId: ({ font_id }) =>
+    Font.findOne({
+      where: { id: font_id },
+      include: [
+        {
+          model: FontTag,
+          as: "fontTags",
+          include: [
+            {
+              model: Tag,
+              as: "tags",
             },
-        }).then((fonts) => fonts),
+          ],
+        },
+        {
+          model: WebFont,
+          as: "webFont",
+        },
+        {
+          model: ImageFont,
+          as: "imageFont",
+        },
+      ],
+    }).then((data) => data),
 
-    getCorporationAll: () =>
-        Font.findAll({
-            attributes: ['corporation'],
-            group: ['corporation'],
-        }).then((corporation) => corporation),
+  getFontAll: () => {
+    const getFonts = Font.findAll({
+      include: [
+        {
+          model: FontTag,
+          as: "fontTags",
+          include: [
+            {
+              model: Tag,
+              as: "tags",
+            },
+          ],
+        },
+        {
+          model: WebFont,
+          as: "webFont",
+        },
+        {
+          model: ImageFont,
+          as: "imageFont",
+        },
+      ],
+    });
+    return getFonts;
+  },
 
-    deleteFontByFontId: async ({ font_id }) => {
-        try {
-            await ImageFont.destroy({ where: { font_id } });
-            await WebFont.destroy({ where: { font_id } });
+  getFontsByTagId: ({ tag_ids }) => {
+    const getFonts = Font.findAll({
+      include: [
+        {
+          model: FontTag,
+          as: "fontTags",
+          include: [
+            {
+              model: Tag,
+              as: "tags",
+            },
+          ],
+          where: {
+            tag_id: {
+              [Op.in]: tag_ids,
+            },
+          },
+        },
+      ],
+    });
 
-            await FontTag.destroy({ where: { font_id } });
+    return getFonts;
+  },
 
-            await Font.destroy({ where: { id: font_id } });
-        } catch (e) {
-            return false;
-        }
+  getFontsByCorpAndText: ({ corporation, text }) =>
+    Font.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${text}%`,
+        },
+        corporation: {
+          [Op.like]: `${corporation ? corporation : "%"}`,
+        },
+      },
+    }).then((fonts) => fonts),
 
-        return true;
-    },
+  getCorporationAll: () =>
+    Font.findAll({
+      attributes: ["corporation"],
+      group: ["corporation"],
+    }).then((corporation) => corporation),
 
-    createFont: async (_, { name, description }) => {
-        const newFont = await Font.create({
-            name,
-            description,
-        });
+  deleteFontByFontId: async ({ font_id }) => {
+    try {
+      await ImageFont.destroy({ where: { font_id } });
+      await WebFont.destroy({ where: { font_id } });
 
-        const font = await Font.findOne({ where: { id } });
-        return font;
-    },
+      await FontTag.destroy({ where: { font_id } });
 
-    updateFont: async (_, { id, name, description }) => {
-        console.log(id);
-        const font = await Font.findOne({ where: { id } });
-        return font;
-    },
+      await Font.destroy({ where: { id: font_id } });
+    } catch (e) {
+      return false;
+    }
 
-    deleteFont: async (_, { id }) => {
-        console.log(id);
-        const oldFont = await Font.destroy({ where: { id } });
-        const font = await Font.findOne({ where: { id } });
-        return font;
-    },
+    return true;
+  },
+
+  createFont: async (_, { name, description }) => {
+    const newFont = await Font.create({
+      name,
+      description,
+    });
+
+    const font = await Font.findOne({ where: { id } });
+    return font;
+  },
+
+  updateFont: async (_, { id, name, description }) => {
+    console.log(id);
+    const font = await Font.findOne({ where: { id } });
+    return font;
+  },
+
+  deleteFont: async (_, { id }) => {
+    console.log(id);
+    const oldFont = await Font.destroy({ where: { id } });
+    const font = await Font.findOne({ where: { id } });
+    return font;
+  },
 };
 
 module.exports = fontORM;
