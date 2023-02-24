4f95410482d84dd0d0270c3aab65ad40735e47c8
ggosugosu <gohsue0@gmail.com>
2023-02-25 04:21:23 +0900

    feat: production graphql introspeciton 삭제

--- client/pages/api/graphql.ts
+++ client/pages/api/graphql.ts
@@ -247,7 +247,8 @@ export default createYoga<{
 }>({
   schema,
   // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
-  graphqlEndpoint: '/api/graphql'
+  graphqlEndpoint: '/api/graphql',
+  graphiql: process.env.NODE_ENV !== 'production'
 })
 //
 // try {
