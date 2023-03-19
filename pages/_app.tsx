933a04ffd2acdd631418ac5c1a5fce141691689f
ggosugosu <gohsue0@gmail.com>
2023-03-19 14:17:16 +0900

    style: eslint 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import type { AppProps } from "next/app";
-import { RecoilRoot } from "recoil";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import Layout from "components/Layout";
-import { GlobalStyle } from "common/globalStyle";
-import "assets/fonts/fonts.css"
-import "common/filterColor.css"
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Layout from 'components/Layout';
+import { GlobalStyle } from 'common/globalStyle';
+import 'assets/fonts/fonts.css';
+import 'common/filterColor.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({ uri: `${process.env.NEXT_PUBLIC_HOST}`, cache: new InMemoryCache() });
+function MyApp({Component, pageProps}: AppProps) {
+  const client = new ApolloClient({uri: `${process.env.NEXT_PUBLIC_HOST}`, cache: new InMemoryCache()});
   return (
     <ApolloProvider client={client}>
       <RecoilRoot>
-      <GlobalStyle />
+        <GlobalStyle />
         <Layout>
           <Component {...pageProps} />
         </Layout>
