f8f7c50d6ea9f3a9ad62b64375508a9ee2d360de
ggosugosu <gohsue0@gmail.com>
2023-08-27 20:46:15 +0900

    feat: 설명 추가

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -40,7 +40,7 @@ const SettingsPage = () => {
     if (data?.status === 'stopped') {
       mutate({action: 'start'}, {
         onSuccess: () => {
-          alert('시작되었습니다.');
+          alert('시작되었습니다. 적용까지 최대 3분의 시간이 소요됩니다.');
           refetch();
         },
         onError: () => {
@@ -69,12 +69,15 @@ const SettingsPage = () => {
             <div>...Loading</div>
             : (
               <>
+                <ul>
+                  <li>불필요한 과금을 막기 위해서 DB의 상태를 관리합니다. 생각보다 돈이 많이 나가더라고요.</li>
+                  <li>한 시간에 두 번씩 DB가 켜져 있으면 끄는 타이머가 돌고 있으므로, 장시간 이용 중에는 갑자기 DB가 꺼질 수 있습니다.</li>
+                  <li>DB가 꺼지면, 다시 [시작하기]를 눌러주세요. 최대 3분이 소요됩니다. 큰 문제 없다면 약 1분 안으로 켜집니다.</li>
+                  <li>정지를 원할 때 [정지 중]인 상태면 꺼진거나 마찬가지니 안심하시고 페이지에서 나가셔도 됩니다.</li>
+                </ul>
                 <h2>
-                  DB 상태
+                  DB 상태 - {DB_STATES_DESCRIPTION[data?.status]}
                 </h2>
-                <p>
-                  {DB_STATES_DESCRIPTION[data?.status]}
-                </p>
                 <ButtonPositive onClick={handleControl}
                                 text={data?.status === 'available' ? '정지하기' : '시작하기'}
                                 enabled={data?.status === 'available' || data?.status === 'stopped'} />
