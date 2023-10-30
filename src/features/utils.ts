d9c6339fc5e063233c063e55eafe0aa061048e46
ggosugosu <gohsue0@gmail.com>
2023-10-31 01:25:52 +0900

    feat: cookie 유틸 추가

--- src/features/utils.ts
+++ src/features/utils.ts
@@ -1,4 +1,15 @@
-import { flow, map } from 'lodash-es';
+import { map } from 'lodash-es';
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
+export const setCookie = (name: string, value: string, options?: any) => {
+  return cookies.set(name, value, { ...options });
+};
+
+export const getCookie = (name: string) => {
+  return cookies.get(name);
+};
 
 export const getSvgUrl = function (svg: string): string {
   return `data:image/svg+xml;charset=utf8,${svg}`;
