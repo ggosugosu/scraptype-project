c205104a8798db6a629ba017892a7fd7dcef213d
ggosugosu <gohsue@inflab.com>
2023-12-10 17:29:19 +0900

    feat: 로그인 버튼 적용

--- src/components/Navigation/Account.tsx
+++ src/components/Navigation/Account.tsx
@@ -1,15 +1,21 @@
 import { isEmpty } from 'lodash-es';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { styleAccountContainer } from './styles.css';
 
 const Account = () => {
   const { data: session } = useSession();
 
   if (isEmpty(session)) {
-    return <button onClick={() => signIn()}>로그인이 필요합니다.</button>;
+    return (
+      <div className={styleAccountContainer}>
+        <span>로그인이 필요합니다.</span>
+        <button onClick={() => signIn()}>로그인</button>
+      </div>
+    );
   }
 
   return (
-    <div>
+    <div className={styleAccountContainer}>
       <span>{session.user?.name} 님</span>
       <button onClick={() => signOut()}>로그아웃</button>
     </div>
---
+++ src/components/Navigation/styles.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css';
+
+export const styleAccountContainer = style({
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
